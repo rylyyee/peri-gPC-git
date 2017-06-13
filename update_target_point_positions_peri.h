4ddec4f6039c0d3dc45421676fb5415c295e7361
Lindsay Waldrop <lindsay.waldrop@nmt.edu>
2017-06-13 11:29:43 -0600

    Initial file add

---
+++ update_target_point_positions_peri.h
@@ -0,0 +1,20 @@
+#ifndef included_update_target_point_positions_peri
+#define included_update_target_point_positions_peri
+
+#include <ibamr/app_namespaces.h>
+#include <ibtk/LDataManager.h>
+#include "parameterfile.h"
+
+/*
+ * Update the positions of the target point specifications.
+ */
+void
+update_target_point_positions_peri(
+    Pointer<PatchHierarchy<NDIM> > hierarchy,
+    LDataManager* l_data_manager,
+    const double current_time,
+    const double dt,
+    ParameterFile &);
+
+#endif //#ifndef included_update_target_point_positions_peri
+
