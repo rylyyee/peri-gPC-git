8ae56c7919acf859f9b20003bf81bc5914b16078
Lindsay Waldrop <lindsay.waldrop@nmt.edu>
2020-06-22 16:57:02 -0400

    Adding some scripts to generate job files

---
+++ setrunperijobs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# What should you space the jobs by?
+countby=5
+endrun=15
+howmany=$((endrun/countby))
+
+for i in `seq 1 $howmany`;
+do
+
+second=$((countby*i))
+first=$((second-(countby-1)))
+echo Print $first to $second runs. 
+
+awk -v var="$first" 'NR==8 {$0="startrun="'"var"'""} 1' runperi.job > temp${i}
+awk -v var="$second" 'NR==9 {$0="endrun="'"var"'""} 1' temp${i} > go${first}to${second}.job
+
+rm temp${i}
+
+#sbatch go${first}to${second}.job
+
+done
