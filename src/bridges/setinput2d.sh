4fb60a00b13983bb78a64501fe4ae0df6176522b
lindsaywaldrop <waldrop@chapman.edu>
2020-10-07 12:14:29 -0400

    Working version on Bridges

--- src/bridges/setinput2d.sh
+++ src/bridges/setinput2d.sh
@@ -26,9 +26,9 @@ Freq=$(awk -v var="$i" 'NR==var' Freq.txt)
 # Writes file to replace line with Freq
 awk -v var="$Freq" 'NR==5 {$0="FRE = "'"var"'"   // Pumping Frequency (Hz)"} 1' input2dw${i} > input2dw${i}f${i}
 # Edits input2d to create different IBlog and visit files and folders
-awk -v var="$i" 'NR==230 {$0="   log_file_name = \"../results/runs/IB2d.log"'"var"'"\"                //"} 1' input2dw${i}f${i} > input2dw${i}f${i}l${i}
-awk -v var="$i" 'NR==236 {$0="   viz_dump_dirname = \"../results/runsruns/viz_IB2d"'"var"'"\"                //"} 1' input2dw${i}f${i}l${i} > input2dw${i}f${i}l${i}y${i}
-awk -v var="$i" 'NR==245 {$0="   data_dump_dirname = \"../results/runs/hier_data_IB2d"'"var"'"\"          //"} 1' input2dw${i}f${i}l${i}y${i} > input2dw${i}f${i}l${i}y${i}j${i}
+awk -v var="$i" 'NR==230 {$0="   log_file_name = \"../results/ibamr/log-files/IB2d.log"'"var"'"\"                //"} 1' input2dw${i}f${i} > input2dw${i}f${i}l${i}
+awk -v var="$i" 'NR==236 {$0="   viz_dump_dirname = \"../results/ibamr/runs/viz_IB2d"'"var"'"\"                //"} 1' input2dw${i}f${i}l${i} > input2dw${i}f${i}l${i}y${i}
+awk -v var="$i" 'NR==245 {$0="   data_dump_dirname = \"../results/ibamr/runs/hier_data_IB2d"'"var"'"\"          //"} 1' input2dw${i}f${i}l${i}y${i} > input2dw${i}f${i}l${i}y${i}j${i}
 test=$Freq
 echo $test
 if [ $(echo " $test <= 0.70" | bc) -eq 1 ]
--- src/matlab/generate_ibamr-files.sh
+++ src/matlab/generate_ibamr-files.sh
@@ -3,6 +3,9 @@
 WD=${1:?Please provide a top directory}
 track=${2:?Please provide a circulatory track}
 
+rm "$WD"/data/ibamr-files/${track}/*
+rm "$WD"/bin/*.vertex "$WD"/bin/*.target "$WD"/bin/*.beam "$WD"/bin/*.spring
+
 module load matlab
 
 matlab -r "generate_${track};exit"
