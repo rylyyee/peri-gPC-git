ec57790ff0096c45e49b7fe5148e8f36b1f55b41
lindsaywaldrop <waldrop@chapman.edu>
2020-12-15 17:37:02 -0800

    Adding cost of transport matlab script.

--- src/matlab/runall_cost.m
+++ src/matlab/runall_cost.m
@@ -1,25 +1,33 @@
 % script for looping over all hier data for calculations
-cd('/Volumes/HelmsDeep/IBAMR/peri-gPC/peri-pinch')
-load('allpara.txt');
+function runall_cost(topdir, track, setparas)
+
+paths.topdir = topdir;
+paths.matlab = strcat(topdir, '/src/matlab');
+paths.ibamr = strcat(topdir, '/results/ibamr/',track,'_runs/');
+paths.parameter = strcat(topdir, '/data/parameters/');
+paths.results = strcat(topdir, '/results/matlab-csv-files/',track,'_results/');
+
+allpara = load(strcat(paths.parameter, 'allpara_',num2str(setparas),'.txt'));
 
 n = length(allpara);
-%n=5;
+n=3;
 cost = zeros(n,2);
 
-for i =1:n
+for run = 1:n
     
-    if allpara(i,3)<=0.7 
+    if allpara(run, 3)<=0.7 
         endtime=350000;
-    elseif allpara(i,3)<1.0
+    elseif allpara(run,3)<1.0
         endtime=300000;
     else
         endtime=250000;
     end
     
-    [cost(i,1),cost(i,2)] = compute_Work_and_Cost_Of_Transport(i,endtime);
+    [cost(run, 1), cost(run, 2)] = compute_Work_and_Cost_Of_Transport(paths, run, endtime);
     
-    cd('/Volumes/HelmsDeep/IBAMR/peri-gPC/peri-pinch')
+
     
 end
-
-csvwrite(['cost_of_transport_',num2str(n),'_',date,'.csv'],cost)
+mkdir(paths.results)
+csvwrite(strcat(paths.results, 'cost_of_transport_',num2str(n),'_',date,'.csv'),cost)
+end
